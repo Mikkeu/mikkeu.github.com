9acc7054ddf461c8a6a8439772fbeb1cb2ac21a4
Mikaela <mikaela.keung@gmail.com>
2020-02-07 11:30:19 -0800

    change processing

--- des157-wi20/js/script.js
+++ des157-wi20/js/script.js
@@ -2,16 +2,29 @@ function setup() {
    var myCanvas = createCanvas (800, 250);
     frameRate(8);
     noStroke();
-    background(random(255), random(255), random(255));
     myCanvas.parent(mySketch)
   }
   
   function draw() {
-    if(mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(random(255), random(255), random(255), random(255));
-    }
-      var circSize = random(200);
-    ellipse(mouseX, mouseY, circSize, circSize);
+    background(0);
+    fill("#FF346A");
+    ellipse(mouseY,mouseX,50,50);
+   if (mouseX<=300 && mouseY<=300){
+     fill(mouseX,mouseX,mouseY);
+     rect(mouseX,mouseY,50,50);
+   }
+   
+   if (mouseX>=200 && mouseY>=200){
+     fill(35,243,123);
+     rect(mouseX,mouseY,50,50);
+   }
+   
+   if (mouseX>=200 && mouseY<=200){
+   fill(random(100),random(135),random(253));
+   rect(mouseX,mouseY,50,50);
+   }
+   if (mouseX<=200 && mouseY>=200){
+    fill(random(255),random(100),random(132));
+    rect(mouseX,mouseY,50,50);
+   }
   }
